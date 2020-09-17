3161b7623b5005bd951319ac2d64ef29cca95f2e
Maserhe <982289931@qq.com>
2020-09-17 12:51:32 +0800

    update

--- 455.分发饼干.cpp
+++ 455.分发饼干.cpp
@@ -9,7 +9,29 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         
+        //一般的贪心 , 将饼干重大到小排序
+        sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
 
+        int sum = 0;
+        int n1 = g.size();
+        int n2 = s.size();  
+
+        int pos = 0;
+        for (int i = 0; i < n1; i ++ ) {
+            while (pos < n2)
+            {
+                if (s[pos] >= g[i]) {
+                    sum ++ ;
+                    pos ++ ;
+                    break ;
+                }
+                else {
+                    pos ++ ;
+                }
+            }
+        }
+        return sum;
     }
 };
 // @lc code=end
