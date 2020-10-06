fda18e2b5c7b5c1fce0658b54a8347943eb069b8
Maserhe <982289931@qq.com>
2020-10-06 21:14:55 +0800

    2020-10-06

--- 836.矩形重叠.cpp
+++ 836.矩形重叠.cpp
@@ -7,9 +7,15 @@
 // @lc code=start
 class Solution {
 public:
+    
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        //         
 
+        bool a = rec1[0] >= rec2[2]; 
+        bool b = rec1[1] >= rec2[3];
+        bool c = rec1[2] <= rec2[0];
+        bool d = rec1[3] <= rec2[1];
+
+        return !(a || b || c || d);
     }
 };
 // @lc code=end
