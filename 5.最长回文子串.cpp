e784f1b84bf9acc1262885a7da48b14a61b2e029
Maserhe <982289931@qq.com>
2020-10-08 18:26:37 +0800

    2020-10-08

--- 5.最长回文子串.cpp
+++ 5.最长回文子串.cpp
@@ -5,17 +5,29 @@
  */
 
 // @lc code=start
+
 class Solution {
 public:
     string longestPalindrome(string s) {
-        // 就是一个 kmp 求 next 数组。
-        vector<int> next(s.size(), 0);
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
         string ans;
-
-        next[0] = 1;
-        for (int i = 0; i < s.size(); i ++ ) {
-            
+        for (int l = 0; l < n; ++l) {   //字符串长度。
+            for (int i = 0; i + l < n; ++i) { 
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = 1;
+                } else if (l == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.size()) {
+                    ans = s.substr(i, l + 1);
+                }
+            }
         }
+        return ans;
     }
 };
 // @lc code=end
---
+++ 852.山脉数组的峰顶索引.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=852 lang=cpp
+ *
+ * [852] 山脉数组的峰顶索引
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+
+    }
+};
+// @lc code=end
+
