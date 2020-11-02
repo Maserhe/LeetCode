35f1c280576aeeb8791103474e820df7d59ef5a3
Maserhe <982289931@qq.com>
2020-11-02 23:33:14 +0800

    2020-11-02

--- 349.两个数组的交集.cpp
+++ 349.两个数组的交集.cpp
@@ -8,16 +8,11 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> rec;
+        unordered_set<int> rec(nums1.begin(), nums1.end());
         unordered_set<int> ans;
 
-        for (int & n : nums1){
-            rec.insert(n);
-        }
-        for (int & n : nums2){
-            if (rec.find(n) != rec.end() && ans.find(n) == ans.end()){
-                    ans.insert(n);
-            }
+        for (int n : nums2){
+            if (rec.find(n) != rec.end()) ans.insert(n);
         }
         
         vector<int> res(ans.begin(), ans.end());
