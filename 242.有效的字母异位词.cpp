8381e0dd02dc8fd6d4d5ad3c75326bf3f836bcf2
Maserhe <982289931@qq.com>
2020-11-22 22:50:11 +0800

    2020-11-22

--- 242.有效的字母异位词.cpp
+++ 242.有效的字母异位词.cpp
@@ -13,20 +13,17 @@ public:
         // 单词数 相同， 字母的个数也要相同。
         // 用两个字母桶
 
-        int bucket1[26] = {0};
-        int bucket2[26] = {0};
-
-        for (auto &tmp :s){
-            bucket1[tmp - 97] ++ ;
+        if (s.size() != t.size()) return false;
+        int bucket[26] = {0};
+        
+        for (int i = 0; i < s.size(); i ++ ) {
+            bucket[t[i] - 'a'] ++;
+            bucket[s[i] - 'a'] -- ;
         }
-
-        for (auto &tmp :t) {
-            bucket2[tmp - 97] ++ ;
-        }
-
         for (int i = 0; i < 26; i ++ ){
-            if (bucket1[i] != bucket2[i]) return false;
+            if (bucket[i] != 0) return false;
         }
+        
         return true;
 
     }
