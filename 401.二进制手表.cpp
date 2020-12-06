579638f6004ade2222150888a061b645911b713b
Maserhe <982289931@qq.com>
2020-12-06 21:46:23 +0800

    2020-12-06

--- 401.二进制手表.cpp
+++ 401.二进制手表.cpp
@@ -8,23 +8,26 @@
 class Solution {
 public:
     vector<string> readBinaryWatch(int num) {
-
-        // 可以 先 分配 小时， 然后分配 分钟。
-        // 1，分配 小时
-
-        for (int i = 0; i <= num && i < 5; i ++ ){
-            // 小时的 分配方式。一共 有 1 << (i - 1) 种 情况。
-            string time;
-            if (i) {
-                for (int j = 1; j < 1 <<(i - 1); j ++ )
-
-            }else
-            {
-                time = "0:"
+        vector<string> res;
+        //直接遍历  0:00 -> 12:00   每个时间有多少1
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (count1(i) + count1(j) == num) {
+                    res.push_back(to_string(i)+":"+
+                                  (j < 10 ? "0"+to_string(j) : to_string(j)));
+                }
             }
-            
         }
-
+        return res;
+    }
+    //计算二进制中1的个数
+    int count1(int n) {
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
     }
 };
 // @lc code=end
