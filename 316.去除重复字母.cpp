5814abf096d07461250bbba5344e06526650302c
Maserhe <982289931@qq.com>
2020-12-20 18:15:01 +0800

    2020-12-20

---
+++ 316.去除重复字母.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=316 lang=cpp
+ *
+ * [316] 去除重复字母
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> vis(26), num(26);
+        for (char ch : s) {
+            num[ch - 'a']++;
+        }
+
+        string stk;
+        for (char ch : s) {
+            if (!vis[ch - 'a']) {
+                while (!stk.empty() && stk.back() > ch) {
+                    if (num[stk.back() - 'a'] > 0) {
+                        vis[stk.back() - 'a'] = 0;
+                        stk.pop_back();
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = 1;
+                stk.push_back(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return stk;
+    }
+};
+// @lc code=end
+
