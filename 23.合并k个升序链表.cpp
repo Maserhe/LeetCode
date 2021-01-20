84bf92f6adff19e4cc48189314e7a1c5f76f5469
Maserhe <982289931@qq.com>
2021-01-21 02:32:25 +0800

    2021-01-21

--- 23.合并k个升序链表.cpp
+++ 23.合并k个升序链表.cpp
@@ -15,6 +15,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+struct cmp{
+    bool operator()(ListNode * a, ListNode * b) {return a->val > b->val;}
+};
+class Solution {
+public:
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        // 使用 优先队列
+        priority_queue<ListNode *, vector<ListNode*>, cmp> q;
+        // 把队头 全部进 栈。
+        for(auto i: lists) if (i) q.push(i);
+
+        ListNode * head = new ListNode(0);
+        ListNode * temp = head;
+        while (q.size()) {
+            ListNode * t = q.top();
+            q.pop();
+            temp->next = t;
+            temp = t;
+            if (t->next) q.push(t->next);
+        }
+        
+        return head->next;
+    }
+};
+/* 方法二
 class Solution {
 public:
 
@@ -61,5 +88,6 @@ ListNode* mergeKLists(vector<ListNode*>& lists) {
         return head;
     } 
 };
+*/
 // @lc code=end
 
