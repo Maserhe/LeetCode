b068e5670e29974e55169c8c9beabf2f4c37104b
Maserhe <982289931@qq.com>
2021-02-14 21:41:23 +0800

    2021-02-14

---
+++ 297.二叉树的序列化与反序列化.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=297 lang=cpp
+ *
+ * [297] 二叉树的序列化与反序列化
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        
+        if (!root) return "#";
+        return to_string(root->val) + " " + serialize(root->left) + " " + serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream is(data);   
+        return deserialize(is);
+    }
+private:    
+    TreeNode* deserialize(istringstream& s) {
+
+        string t;
+        s >> t;
+        if (t == "#") return NULL;
+        TreeNode* node = new TreeNode(stoi(t));
+        node->left = deserialize(s);
+        node->right = deserialize(s);
+        return node;
+    }
+
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
+// @lc code=end
+
