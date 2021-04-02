8294292d60103748097337674d17120fba3c1179
Maserhe <982289931@qq.com>
2021-04-02 23:00:56 +0800

    2021-04-02

--- 42.接雨水.java
+++ 42.接雨水.java
@@ -8,8 +8,22 @@
 class Solution {
     public int trap(int[] height) {
 
+        int ans = 0;
+        Stack<Integer> st = new Stack<>();
 
+        for (int i = 0; i < height.length; i ++ ) {
 
+            while (!st.isEmpty() && height[st.peek()] < height[i]) {
+                int first = st.pop();
+                if (st.isEmpty()) break;
+                int second = st.peek();
+                int h = Math.min(height[i], height[second]) - height[first];
+                int w = i - second - 1;
+                ans += (h * w);
+            }
+            st.add(i);
+        }
+        return ans;
     }
 }
 // @lc code=end
