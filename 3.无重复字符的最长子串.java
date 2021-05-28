7ab98246406fd9a617654072fef25effbcdd3de5
Maserhe <982289931@qq.com>
2021-05-28 22:04:56 +0800

    2021-05-28

--- 3.无重复字符的最长子串.java
+++ 3.无重复字符的最长子串.java
@@ -9,15 +9,15 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
 
         int ans = 0;
-        int[] arr = new int[256];
-        for (int i = 0; i < arr.length; i ++ ) arr[i] = -1;
         int start = -1;
+        int[] arr = new int[265];
+        for (int i = 0; i < arr.length; i ++ ) arr[i] = -1;
+
         for (int i = 0; i < s.length(); i ++ ) {
             if (arr[s.charAt(i)] > start) start = arr[s.charAt(i)];
             arr[s.charAt(i)] = i;
             ans = Math.max(ans, i - start);
         }
-
         return ans;
     }
 }
---
+++ 5.最长回文子串.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        int max = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i ++ ) {
+            int len = Math.max(getLen(i, i + 1, s), getLen(i, i, s));
+            if (len > max) {
+                max = len;
+                start = i - ((len - 1) >> 1);
+            }
+        }
+        String ans="";
+        for (int i = 0; i < max; i ++ ) {
+            ans += s.charAt(i + start);
+        }
+        return ans;
+    }
+
+    public int getLen(int l, int r, String s) {
+        int n = s.length();
+        while (l > 0 && r < n && s.charAt(l) == s.charAt(r)) {
+            l -- ;
+            r ++ ;
+        }
+        return r - l - 1;
+    }
+}
+// @lc code=end
+
